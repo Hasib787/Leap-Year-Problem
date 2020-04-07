ee65ddade3e0f343a3f005528d66021a45d4e3e8
Md. Masud Rana <mr.hasib45@gmail.com>
2020-04-07 22:23:47 +0600

    Create leap-year.js

---
+++ leap-year.js
@@ -0,0 +1,13 @@
+let year=2016;
+
+  if(year%400==0){
+    console.log("This is a leap Year");
+  }
+  else if(year%100==0){
+    console.log("This is not a leap year");
+  }
+   else if(year%4==0){
+    console.log("This is a leap year");}
+   else{
+    console.log("Is not a leap year");
+  }
